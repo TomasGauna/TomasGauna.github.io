8ca42f4de53f26e234126989482177fa2055ed65
TomasGauna <tgauna@netnix.net>
2024-04-30 10:58:17 -0300

    box de proyectos

--- app/components/Experiencia/Experiencia.tsx
+++ app/components/Experiencia/Experiencia.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import Box from '../Box/Box';
 
 function Experiencia() {
+  const projects = [
+  {
+    title: "Proyecto 1",
+    description: "Una breve descripción del proyecto 1.",
+    images: ["/iconos/linkedin.png", "/iconos/github.png"],
+    repoLink: "https://github.com/example/repo1"
+  },
+  {
+    title: "Proyecto 2",
+    description: "Una breve descripción del proyecto 2.",
+    images: ["/iconos/github.png"],
+    repoLink: "https://github.com/example/repo2"
+  },
+];
+
   return (
     <div>
-      Experiencia
+      <h1>Mis Proyectos</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {projects.map((project, index) => (
+          <Box key={index} {...project} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Experiencia
