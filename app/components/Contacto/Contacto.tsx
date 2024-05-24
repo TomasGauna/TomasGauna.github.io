01f7659b035b3d6cbf969a0fb7eed67ff3c64efc
TomasGauna <tgauna@netnix.net>
2024-05-24 10:46:37 -0300

    corrijo num de telefono

--- app/components/Contacto/Contacto.tsx
+++ app/components/Contacto/Contacto.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import React from 'react';
 
 function Contacto() {
-  const numeroTelefono = "+54 388-500-5210";
+  const numeroTelefono = "+549 388-500-5210";
   const email = "tomi.gauna08@gmail.com";
 
   return (
@@ -12,7 +12,7 @@ function Contacto() {
         <p className='text-5xl lg:text-6xl text-center mb-10'>Contacto</p>
         <div className='flex flex-wrap justify-center items-center gap-32'>
           <div className="flex flex-col gap-px items-center mt-10">
-            <Link href="https://wa.me/+543885005210" target="_blank" rel="noopener noreferrer">
+            <Link href="https://wa.me/+5493885005210" target="_blank" rel="noopener noreferrer">
               <Image src="/contacto/whatsapp.png" width={32} height={32} alt="WhatsApp" className="w-32 h-32 mb-2 hover:scale-105 transition-transform active:scale-100" />
             </Link>
             <p className="text-lg">{numeroTelefono}</p>
