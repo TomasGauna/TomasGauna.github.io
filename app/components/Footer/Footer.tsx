eaaa5e137092f840bc03bfb4e66ca94974b4a777
TomasGauna <tgauna@netnix.net>
2024-05-24 10:13:51 -0300

    hover scale redes sociales

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -7,21 +7,21 @@ function Footer() {
     <footer className='border-t border-t-gray-200 pt-10 text-base'>
         <div className='flex flex-col items-center justify-center'>
             <p className='mb-5'>Redes sociales</p>
-            <div className='flex justify-center gap-4'>
+            <div className='flex justify-center gap-6'>
                 <Link href={"https://www.linkedin.com/in/tomas-agustin-gauna-373290292/"}>
-                    <Image src={"/iconos/linkedin.png"} alt='' height={60} width={60} />
+                    <Image src={"/iconos/linkedin.png"} alt='' height={60} width={60} className='hover:scale-105 transition-transform'/>
                 </Link>
 
                 <Link href={"https://github.com/TomasGauna/"}>
-                    <Image src={"/iconos/github.png"} alt='' height={60} width={60} />
+                    <Image src={"/iconos/github.png"} alt='' height={60} width={60} className='hover:scale-105 transition-transform'/>
                 </Link>
 
                 <Link href={"https://www.instagram.com/tomi_atienzo/"}>
-                    <Image src={"/iconos/instagram.png"} alt='' height={60} width={60} />
+                    <Image src={"/iconos/instagram.png"} alt='' height={60} width={60} className='hover:scale-105 transition-transform'/>
                 </Link>
 
                 <Link href={"https://x.com/tomi_atienzo?s=11"}>
-                    <Image src={"/iconos/twitter.png"} alt='' height={60} width={60}/>
+                    <Image src={"/iconos/twitter.png"} alt='' height={60} width={60} className='hover:scale-105 transition-transform'/>
                 </Link>
             </div>
         </div>
